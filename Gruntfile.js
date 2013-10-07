b1784fe77956d6aab7e13e9b271f489aed6b803a
Nick Fisher <Nick.Fisher@tstllc.net>
2013-10-07 00:11:13 -0400

    finally got mocha working with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,42 @@
+'use strict';
+
 module.exports = function(grunt) {
     //grunt plugins
-
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
+    // TODO: JSHint
 
     grunt.initConfig({
-        min: {
-            maps: 'knockout-angular-maps.js'
+        copy: {
+            dist: {
+                files: [
+
+                ]
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                background: true
+            },
+            continuous: {
+                configFile: 'karma.conf.js',
+                singleRun: true,
+                browsers: ['PhantomJS']
+            }
+        },
+        watch: {
+            karma: {
+                files: ['knockout-google-maps.js', 'spec/**.js'],
+                tasks: ['karma:unit:run']
+            },
+            js: {
+                files: ['knockout-google-maps.js'],
+                tasks: []
+            }
         }
     });
 };
